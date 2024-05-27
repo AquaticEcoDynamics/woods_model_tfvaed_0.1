3ab7226d03224c55fbfe53e81057f84b2923b0d1
Brendan Busch <brendan.busch@uwa.edu.au>
2024-05-27 13:48:17 +0800

    Updates to naming & long wave model default

--- model_runs/HD/runmodel.bat
+++ model_runs/HD/runmodel.bat
@@ -1,1 +1,3 @@
-Y:\Testing\TFV-AED\2023.1.1\TUFLOWFV.exe woods_2023_2024_test.fvc
+REM Y:\Testing\TFV-AED\2023.1.1\TUFLOWFV.exe woods_2023_2024_test.fvc
+
+Y:\Testing\TFV-AED\2023.1.1\TUFLOWFV.exe woods_2015_2017_test.fvc
